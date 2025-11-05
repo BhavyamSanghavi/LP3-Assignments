458cbdc24a9e0f890a5a5d24e4b544a9692ac366
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2025-11-05 15:13:58 +0530

    Completed

--- DAA03-Knapsack.cpp
+++ DAA03-Knapsack.cpp
@@ -3,21 +3,41 @@
 #include <algorithm>
 using namespace std;
 
-int knapsack(int W, vector<int> &wt, vector<int> &val) {
-    int n = wt.size();
-    vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
-
-    for (int i = 1; i <= n; i++) {
-        for (int w = 1; w <= W; w++) {
-            if (wt[i - 1] <= w)
-                dp[i][w] = max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w]);
-            else
-                dp[i][w] = dp[i - 1][w];
-        }
+int solve(int W, int wt[],int val[], int n, int i,vector<vector<int>>&dp)
+    {
+        if(i>=n || W<=0) return 0;
+        if(dp[i][W]!=-1) return dp[i][W];
+        int pick=0;
+        if(W-wt[i]>=0)
+        pick+=val[i]+solve(W-wt[i],wt,val,n,i+1,dp);
+        int nopick=solve(W,wt,val,n,i+1,dp);
+        
+        return dp[i][W]=max(pick,nopick);
     }
-
-    return dp[n][W];
-}
+    
+    int solveTab(int W,int wt[],int val[], int n)
+    {
+        vector<vector<int>>dp(n+1,vector<int>(W+1,0));
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int w=0;w<=W;w++)
+            {
+                int pick=0;
+                if(w-wt[i]>=0)
+                pick=val[i]+dp[i+1][w-wt[i]];
+                int nopick=dp[i+1][w];
+                dp[i][w]=max(pick,nopick);
+            }
+        }
+        return dp[0][W];
+    }
+    
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+        // vector<vector<int>>dp(n+1,vector<int>(W+1,-1));
+        //   return solve(W,wt,val,n,0,dp);
+        return solveTab(W,wt,val,n);
+    }
 
 int main() {
     int W = 269; // Capacity
